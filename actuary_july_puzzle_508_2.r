317b3ec4b5fca70390738a4cfd337f264eb2b6ba
Matt Malin <email@mattmalin.co.uk>
2012-07-07 14:21:08 +0100

    tidying up main loop code

--- actuary_july_puzzle_508_2.r
+++ actuary_july_puzzle_508_2.r
@@ -27,7 +27,6 @@ for(n_value in possible_n) {
 	  possible_p <- possible_p[!{possible_p %in% c(n_value, o_value, m_value, q_value, l_value, s_value, k_value, u_value, t_value)}] # uniques
 	  if(length(possible_p) == 0) next
 	  for(p_value in possible_p) {
-	    message("n: ", n_value, ", o: ", o_value, ", s: ", s_value, ", p: ", p_value)
 	    r_value <- l_value + t_value - n_value - p_value
 		j_value <- r_value - p_value
 		i_value <- j_value - p_value
@@ -39,53 +38,21 @@ for(n_value in possible_n) {
 		c_value <- d_value + e_value
 		b_value <- c_value + d_value
 		a_value <- b_value + f_value
-        if(any(duplicated(c(
-          a = a_value, 
-          b = b_value, 
-          c = c_value, 
-          d = d_value, 
-          e = e_value, 
-          f = f_value, 
-          g = g_value, 
-          h = h_value, 
-          i = i_value, 
-          j = j_value, 
-          k = k_value, 
-          l = l_value, 
-          m = m_value, 
-          n = n_value, 
-          o = o_value, 
-          p = p_value, 
-          q = q_value, 
-          r = r_value, 
-          s = s_value, 
-          t = t_value, 
-          u = u_value)))) next
+		
+        all_values <- c(
+		  a = a_value, b = b_value, c = c_value, d = d_value, e = e_value, 
+		  f = f_value, g = g_value, h = h_value, i = i_value, j = j_value, 
+          k = k_value, l = l_value, m = m_value, n = n_value, o = o_value, 
+          p = p_value, q = q_value, r = r_value, s = s_value, t = t_value, 
+		  u = u_value)
+		  
+		if(any(duplicated(all_values))) next
 		if(a_value + b_value + c_value != u_value + t_value + r_value) next
 		if(b_value + c_value != k_value + u_value) next
 		if(b_value + t_value != k_value + e_value + j_value) next
-		looped_possible[[i]] <- c(
-          a = a_value, 
-          b = b_value, 
-          c = c_value, 
-          d = d_value, 
-          e = e_value, 
-          f = f_value, 
-          g = g_value, 
-          h = h_value, 
-          i = i_value, 
-          j = j_value, 
-          k = k_value, 
-          l = l_value, 
-          m = m_value, 
-          n = n_value, 
-          o = o_value, 
-          p = p_value, 
-          q = q_value, 
-          r = r_value, 
-          s = s_value, 
-          t = t_value, 
-          u = u_value)
+		
+		looped_possible[[i]] <- all_values
+		  
 	    print(looped_possible[[i]])
 		i <- i + 1
 	  }
