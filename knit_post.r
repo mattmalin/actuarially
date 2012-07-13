2721e1b60855d761d5a0a31e86331d0ff0c92edd
Matt Malin <email@mattmalin.co.uk>
2012-07-14 00:38:34 +0100

    smartphone_split.Rmd

--- knit_post.r
+++ knit_post.r
@@ -4,3 +4,7 @@ library(markdown)
 knit2html("actuary_april_2012.Rmd", "actuary_april_2012.html")
 browseURL("actuary_april_2012.html")
 
+knit2html("smartphone_split.r", "smartphone_split.html")
+browseURL("smartphone_split.html")
+
+
