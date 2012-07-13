73972f5dcec41475ee7dc710c2f6bc07d40edcb2
Matt Malin <email@mattmalin.co.uk>
2012-07-14 00:45:24 +0100

    smartphone operating system share post finished (posted)

--- knit_post.r
+++ knit_post.r
@@ -4,6 +4,9 @@ library(markdown)
 knit2html("actuary_april_2012.Rmd", "actuary_april_2012.html")
 browseURL("actuary_april_2012.html")
 
+knit("smartphone_split.Rmd", "smartphone_split.Md")
+markdownToHTML("smartphone_split.Md", "smartphone_split.html", fragment.only = TRUE)
+
 knit2html("smartphone_split.r", "smartphone_split.html")
 browseURL("smartphone_split.html")
 
